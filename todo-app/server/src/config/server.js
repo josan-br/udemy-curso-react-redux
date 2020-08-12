13cf19a927c8c3eca8144bdff7144557fec198ea
josan-br <josantnun@outlook.com>
2020-08-12 20:44:12 -0300

    habilitando o cors

--- todo-app/server/src/config/server.js
+++ todo-app/server/src/config/server.js
@@ -6,8 +6,11 @@ const express = require("express");
 
 const server = express();
 
+const cors = require("./cors");
+
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(cors);
 
 server.listen(PORT, function () {
   console.log(`BACKEND is running on port ${PORT}`);
