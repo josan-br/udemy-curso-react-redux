03fc22d7eab0e25733925be98aeef7233712f332
josan-br <josantnun@outlook.com>
2020-08-16 21:58:45 -0300

    TodoApp (frontend), Concluido

--- todo-app/client/src/template/pageHeader.jsx
+++ todo-app/client/src/template/pageHeader.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default (props) => {
+export default (props) => (
   <header className="page-header">
     <h2>
       {props.name} <small>{props.small}</small>
     </h2>
-  </header>;
-};
+  </header>
+);
